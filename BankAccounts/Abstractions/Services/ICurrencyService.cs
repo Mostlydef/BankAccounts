b51d9976d001faebd92184970c0e4b9daf73808f
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:00:33 +0300

    добавил заглушку сервиса валют

---
+++ BankAccounts/Abstractions/Services/ICurrencyService.cs
@@ -0,0 +1,15 @@
+﻿namespace BankAccounts.Abstractions.Services
+{
+    /// <summary>
+    /// Сервис для проверки поддержки валюты.
+    /// </summary>
+    public interface ICurrencyService
+    {
+        /// <summary>
+        /// Проверяет, поддерживается ли указанная валюта.
+        /// </summary>
+        /// <param name="currencyCode">Трёхбуквенный код валюты в формате ISO.</param>
+        /// <returns>Возвращает <c>true</c>, если валюта поддерживается, иначе <c>false</c>.</returns>
+        public bool IsSupported(string currencyCode);
+    }
+}
---
+++ BankAccounts/Infrastructure/CurrencyService/CurrencyServiceStub.cs
@@ -0,0 +1,32 @@
+﻿using BankAccounts.Abstractions.Services;
+
+namespace BankAccounts.Infrastructure.CurrencyService
+{
+    /// <summary>
+    /// Заглушка сервиса валют, предоставляющая проверку поддерживаемых валют.
+    /// Используется для тестирования и отладки без подключения к внешним API.
+    /// </summary>
+    public class CurrencyServiceStub : ICurrencyService
+    {
+        /// <summary>
+        /// Заглушка сервиса валют, предоставляющая проверку поддерживаемых валют.
+        /// Используется для тестирования и отладки без подключения к внешним API.
+        /// </summary>
+        private readonly HashSet<string> _supportedCurrencies =
+        [
+            "USD", "EUR", "RUB", "JPY"
+        ];
+
+        /// <summary>
+        /// Проверяет, поддерживается ли валюта по её коду.
+        /// </summary>
+        /// <param name="currencyCode">Код валюты (например, "USD", "EUR").</param>
+        /// <returns>
+        /// <c>true</c>, если валюта поддерживается; в противном случае — <c>false</c>.
+        /// </returns>
+        public bool IsSupported(string currencyCode)
+        {
+            return _supportedCurrencies.Contains(currencyCode);
+        }
+    }
+}
