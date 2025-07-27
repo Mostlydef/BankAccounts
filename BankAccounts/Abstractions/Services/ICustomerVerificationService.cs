32fc1bb9793d334f2300774149655abac486b353
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:00:59 +0300

    добавил заглушку сервиса верификации клиентов

---
+++ BankAccounts/Abstractions/Services/ICustomerVerificationService.cs
@@ -0,0 +1,16 @@
+﻿namespace BankAccounts.Abstractions.Services
+{
+    /// <summary>
+    /// Сервис для проверки существования владельца счета.
+    /// </summary>
+    public interface ICustomerVerificationService
+    {
+        /// <summary>
+        /// Проверяет, существует ли владелец с указанным идентификатором.
+        /// </summary>
+        /// <param name="ownerId">Идентификатор владельца.</param>
+        /// <param name="cancellation">Токен отмены операции.</param>
+        /// <returns>Возвращает <c>true</c>, если владелец существует, иначе <c>false</c>.</returns>
+        public Task<bool> OwnerExistsAsync(Guid ownerId, CancellationToken cancellation);
+    }
+}
---
+++ BankAccounts/Infrastructure/VerificationService/CustomerVerificationServiceStub.cs
@@ -0,0 +1,25 @@
+﻿using BankAccounts.Abstractions.Services;
+
+namespace BankAccounts.Infrastructure.VerificationService
+{
+    /// <summary>
+    /// Заглушка сервиса проверки существования владельца счёта.
+    /// Используется для имитации проверки без обращения к реальному хранилищу данных.
+    /// </summary>
+    public class CustomerVerificationServiceStub : ICustomerVerificationService
+    {
+        /// <summary>
+        /// Проверяет, существует ли владелец счёта.
+        /// </summary>
+        /// <param name="ownerId">Идентификатор владельца счёта.</param>
+        /// <param name="cancellation">Токен отмены операции.</param>
+        /// <returns>
+        /// Возвращает <c>true</c>, если <paramref name="ownerId"/> не равен <see cref="Guid.Empty"/>, иначе <c>false</c>.
+        /// </returns>
+        public Task<bool> OwnerExistsAsync(Guid ownerId, CancellationToken cancellation)
+        {
+            _ = cancellation;
+            return Task.FromResult(ownerId != Guid.Empty);
+        }
+    }
+}
