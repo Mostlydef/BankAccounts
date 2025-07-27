19e380bfe8aaf8bdb7619d9e51ca2dd2c2df1b4f
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:06:13 +0300

    добавил запрос для получения всех счетов по идентификатору владельца

---
+++ BankAccounts/Features/Accounts/GetAllAccount/GetAllAccountByOwnerIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using BankAccounts.Abstractions.CQRS;
+using BankAccounts.Features.Accounts.DTOs;
+
+namespace BankAccounts.Features.Accounts.GetAllAccount
+{
+    /// <summary>
+    /// Запрос для получения всех счетов по идентификатору владельца.
+    /// </summary>
+    /// <param name="OwnerId">Идентификатор владельца счетов.</param>
+    public record GetAllAccountByOwnerIdQuery(Guid OwnerId) : IQuery<List<AccountDto>>;
+}
