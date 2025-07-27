a9b50b790079e218c9ae730dc5b4c870771a9cb4
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:07:25 +0300

    добавил команду для изменения процентной ставки

---
+++ BankAccounts/Features/Accounts/UpdateAccountInterestRate/UpdateInterestRateCommand.cs
@@ -0,0 +1,12 @@
+﻿using BankAccounts.Abstractions.CQRS;
+using BankAccounts.Features.Accounts.DTOs;
+
+namespace BankAccounts.Features.Accounts.UpdateAccountInterestRate
+{
+    /// <summary>
+    /// Команда для обновления процентной ставки по счету.
+    /// </summary>
+    /// <param name="AccountId">Идентификатор счета, для которого обновляется процентная ставка.</param>
+    /// <param name="InterestRateDto">DTO с новым значением процентной ставки.</param>
+    public record UpdateInterestRateCommand(Guid AccountId, AccountInterestRateDto InterestRateDto) : ICommand<bool>;
+}
---
+++ BankAccounts/Features/Accounts/UpdateAccountInterestRate/UpdateInterestRateCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BankAccounts.Abstractions.CQRS;
+using BankAccounts.Database.Interfaces;
+
+namespace BankAccounts.Features.Accounts.UpdateAccountInterestRate
+{
+    /// <summary>
+    /// Обработчик команды обновления процентной ставки по счету.
+    /// </summary>
+    public class UpdateInterestRateCommandHandler : ICommandHandler<UpdateInterestRateCommand, bool>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        /// <summary>
+        /// Создает новый экземпляр <see cref="UpdateInterestRateCommandHandler"/>.
+        /// </summary>
+        /// <param name="accountRepository">Репозиторий для работы со счетами.</param>
+        public UpdateInterestRateCommandHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        /// <summary>
+        /// Обрабатывает команду обновления процентной ставки.
+        /// </summary>
+        /// <param name="request">Команда с данными для обновления ставки.</param>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
+        /// <returns>Возвращает <c>true</c>, если обновление выполнено успешно; иначе <c>false</c>.</returns>
+        public async Task<bool> Handle(UpdateInterestRateCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.AccountId, cancellationToken);
+            if (account == null)
+            {
+                return false;
+            } 
+            account.InterestRate = request.InterestRateDto.InterestRate;
+
+            await _accountRepository.UpdateAsync(account);
+            return true;
+        }
+    }
+}
