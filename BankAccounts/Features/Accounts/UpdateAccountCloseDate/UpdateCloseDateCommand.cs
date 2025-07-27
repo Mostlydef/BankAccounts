35bdede89afb96d0b9a4e7d751c6dc5fab402c69
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:07:00 +0300

    добавил команду для изменения даты закрытия счета

---
+++ BankAccounts/Features/Accounts/UpdateAccountCloseDate/UpdateCloseDateCommand.cs
@@ -0,0 +1,12 @@
+﻿using BankAccounts.Abstractions.CQRS;
+using BankAccounts.Features.Accounts.DTOs;
+
+namespace BankAccounts.Features.Accounts.UpdateAccountCloseDate
+{
+    /// <summary>
+    /// Команда для обновления даты закрытия счета.
+    /// </summary>
+    /// <param name="AccountId">Идентификатор счета, для которого обновляется дата закрытия.</param>
+    /// <param name="CloseDateDto">DTO с новой датой закрытия счета.</param>
+    public record UpdateCloseDateCommand(Guid AccountId, AccountCloseDateDto CloseDateDto) : ICommand<bool>;
+}
