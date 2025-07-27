875d3259163a6e035ab9f7722e0e1580ee4a2dbd
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 18:55:52 +0300

    добавил DTO для модели Account

---
+++ BankAccounts/Features/Accounts/DTOs/AccountCloseDateDto.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace BankAccounts.Features.Accounts.DTOs
+{
+    /// <summary>
+    /// DTO для обновления даты закрытия счета.
+    /// </summary>
+    public class AccountCloseDateDto
+    {
+        /// <summary>
+        /// Дата закрытия счета.
+        /// </summary>
+        public DateTime CloseDate { get; [UsedImplicitly] set; }
+    }
+}
---
+++ BankAccounts/Features/Accounts/DTOs/AccountInterestRateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BankAccounts.Features.Accounts.DTOs
+{
+    /// <summary>
+    /// DTO для передачи информации о процентной ставке по счету.
+    /// </summary>
+    public class AccountInterestRateDto
+    {
+        /// <summary>
+        /// Процентная ставка по счету.
+        /// </summary>
+        public decimal InterestRate { get; set; }
+    }
+}
