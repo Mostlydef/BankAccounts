51f8b394f941f6d9cfea4115df43d9fc45d12583
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 18:56:18 +0300

    добавил DTO для модели Transaction

---
+++ BankAccounts/Features/Transactions/DTOs/TransactionCreateDto.cs
@@ -0,0 +1,33 @@
+﻿namespace BankAccounts.Features.Transactions.DTOs
+{
+    /// <summary>
+    /// DTO для создания новой транзакции.
+    /// </summary>
+    public class TransactionCreateDto
+    {
+        /// <summary>
+        /// Идентификатор счёта, на котором создается транзакция.
+        /// </summary>
+        public Guid AccountId { get; set; }
+        /// <summary>
+        /// Идентификатор контрагента (если применимо).
+        /// </summary>
+        public Guid? CounterpartyAccountId { get; set; }
+        /// <summary>
+        /// Сумма транзакции.
+        /// </summary>
+        public decimal Amount { get; set; }
+        /// <summary>
+        /// Валюта транзакции, например "USD", "EUR".
+        /// </summary>
+        public required string Currency { get; set; }
+        /// <summary>
+        /// Тип транзакции. Значения: "Credit" или "Debit".
+        /// </summary>
+        public required string Type { get; init; } = string.Empty;
+        /// <summary>
+        /// Описание транзакции.
+        /// </summary>
+        public required string Description { get; init; } = string.Empty;
+    }
+}
---
+++ BankAccounts/Features/Transactions/DTOs/TransactionDto.cs
@@ -0,0 +1,41 @@
+﻿namespace BankAccounts.Features.Transactions.DTOs
+{
+    /// <summary>
+    /// DTO для передачи данных о транзакции.
+    /// </summary>
+    public class TransactionDto
+    {
+        /// <summary>
+        /// Уникальный идентификатор транзакции.
+        /// </summary>
+        public Guid Id { get; init; }
+        /// <summary>
+        /// Идентификатор счёта, к которому относится транзакция.
+        /// </summary>
+        public Guid AccountId { get; init; }
+        /// <summary>
+        /// Идентификатор контрагента (если применимо).
+        /// </summary>
+        public Guid? CounterpartyAccountId { get; init; }
+        /// <summary>
+        /// Сумма транзакции.
+        /// </summary>
+        public decimal Amount { get; init; }
+        /// <summary>
+        /// Валюта транзакции, например, "USD", "RUB".
+        /// </summary>
+        public required string Currency { get; init; }
+        /// <summary>
+        /// Тип транзакции — "Credit" или "Debit".
+        /// </summary>
+        public required string Type { get; init; }
+        /// <summary>
+        /// Описание транзакции.
+        /// </summary>
+        public required string Description { get; init; }
+        /// <summary>
+        /// Время создания транзакции.
+        /// </summary>
+        public DateTime Timestamp { get; init; }
+    }
+}
