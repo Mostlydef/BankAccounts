9a361b871638e1ef04b52ba6413406b6d31b98ec
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:09:29 +0300

    добавил контроллер для модели Transaction

---
+++ BankAccounts/Features/Transactions/TransactionsController.cs
@@ -0,0 +1,59 @@
+﻿using BankAccounts.Features.Transactions.CreateTransaction;
+using BankAccounts.Features.Transactions.CreateTransfer;
+using BankAccounts.Features.Transactions.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAccounts.Features.Transactions
+{
+    /// <summary>
+    /// Контроллер для работы с транзакциями.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="TransactionsController"/>.
+        /// </summary>
+        /// <param name="mediator">Интерфейс медиатора для обработки запросов и команд.</param>
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Регистрирует одну транзакцию (списание или пополнение).
+        /// </summary>
+        /// <param name="createDto">Данные для создания транзакции.</param>
+        /// <returns>Созданная транзакция или ошибка.</returns>
+        /// <response code="200">Успешное создание транзакции.</response>
+        /// <response code="400">Ошибка валидации данных или бизнес-логики.</response>
+        [HttpPost]
+        public async Task<IActionResult> RegisterTransaction([FromBody] TransactionCreateDto createDto)
+        {
+            var transactionDto = await _mediator.Send(new CreateTransactionCommand(createDto));
+            if (transactionDto == null)
+            {
+                return BadRequest("Ошибка при создании транзакции.");
+            }
+            return Ok(transactionDto);
+        }
+
+        /// <summary>
+        /// Переводит деньги между двумя счетами.
+        /// </summary>
+        /// <param name="transactionCreateDto">Данные перевода (откуда, куда, сумма, валюта и т.д.).</param>
+        /// <returns>Результат перевода (дебет и кредит).</returns>
+        /// <response code="200">Перевод выполнен успешно.</response>
+        /// <response code="400">Ошибка перевода (например, недостаточно средств, неподдерживаемая валюта).</response>
+        [HttpPost("Transfer")]
+        public async Task<IActionResult> Trasnfer([FromBody] TransactionCreateDto transactionCreateDto)
+        {
+            var result = await _mediator.Send(new CreateTransferCommand(transactionCreateDto));
+            return Ok(result);
+        }
+    }
+}
