146bb5d444641daa296e82d57f11ca5fd3fb30ff
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 18:58:40 +0300

    добавли перечисляемые типы AccountType и TransactionType

---
+++ BankAccounts/Features/Accounts/AccountType.cs
@@ -0,0 +1,21 @@
+﻿namespace BankAccounts.Features.Accounts
+{
+    /// <summary>
+    /// Типы счетов, доступные в системе банка.
+    /// </summary>
+    public enum AccountType
+    {
+        /// <summary>
+        /// Текущий счет (Checking account).
+        /// </summary>
+        Checking,
+        /// <summary>
+        /// Депозитный счет (Deposit account).
+        /// </summary>
+        Deposit,
+        /// <summary>
+        /// Кредитный счет (Credit account).
+        /// </summary>
+        Credit
+    }
+}
---
+++ BankAccounts/Features/Transactions/TransactionType.cs
@@ -0,0 +1,17 @@
+﻿namespace BankAccounts.Features.Transactions
+{
+    /// <summary>
+    /// Определяет тип транзакции.
+    /// </summary>
+    public enum TransactionType
+    {
+        /// <summary>
+        /// Кредитовая транзакция — поступление средств на счёт.
+        /// </summary>
+        Credit,
+        /// <summary>
+        /// Дебетовая транзакция — списание средств со счёта.
+        /// </summary>
+        Debit
+    }
+}
