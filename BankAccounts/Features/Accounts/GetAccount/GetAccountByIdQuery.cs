14cc19bcd0de3852d3ff4b0ab8875ce370743f0a
Mostlydef <v.danilevitch2013@yandex.ru>
2025-07-27 19:05:18 +0300

    добавли запрос для получения счета по идентификатору

---
+++ BankAccounts/Features/Accounts/GetAccount/GetAccountByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using BankAccounts.Abstractions.CQRS;
+using BankAccounts.Features.Accounts.DTOs;
+
+namespace BankAccounts.Features.Accounts.GetAccount
+{
+    /// <summary>
+    /// Запрос на получение информации о счёте по его идентификатору.
+    /// </summary>
+    /// <param name="AccountId">Идентификатор счёта.</param>
+    public record GetAccountByIdQuery(Guid AccountId) : IQuery<AccountDto?>;
+}
